4a355dbb3edf8c71e37e460bd775b794a4994d27
Mr. Marvin Jupiter Roilo Vargas <mvargas2@my.centennialcollege.ca>
2017-12-20 16:32:41 -0500

    Complete Lab5

---
+++ COMP305-F2017/Assets/Scripts/Lab5/CollectedItemView.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CollectedItemView : MonoBehaviour {
+
+    public GameObject itemList;
+    public GameObject itemPrefab;
+
+    void Start()
+    {
+        Dictionary<Sprite, int> itemCount = Collectitems.GlobalInstance.itemCount;
+
+        foreach (Sprite s in itemCount.Keys)
+        {
+            GameObject item = Instantiate(itemPrefab, itemList.transform);
+
+            item.GetComponentInChildren<Image>().sprite = s;
+            item.GetComponentInChildren<Text>().text = "" + itemCount[s];
+        }
+    }
+}
---
+++ COMP305-F2017/Assets/Scripts/Lab5/Collectitems.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectitems : MonoBehaviour {
+
+    private static Collectitems _globalInstance;
+
+    public static Collectitems GlobalInstance
+    {
+        get
+        {
+            if (_globalInstance == null)
+            {
+                Debug.Log("No instance found");
+            }
+            return _globalInstance;
+        }
+    }
+
+    public Dictionary<Sprite, int> itemCount;
+
+    void Start()
+    {
+        itemCount = new Dictionary<Sprite, int>();
+    }
+
+    public void CollectedItem(GameObject obj)
+    {
+        Sprite s = obj.GetComponent<SpriteRenderer>().sprite;
+
+        if (!itemCount.ContainsKey(s))
+        {
+            itemCount.Add(s, 0);
+        }
+
+        itemCount[s] += 1;
+    }
+
+    private void Awake()
+    {
+        if (_globalInstance != null && _globalInstance != this)
+        {
+            Debug.Log("Multiple instances found");
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _globalInstance = this;
+        }
+    }
+}
