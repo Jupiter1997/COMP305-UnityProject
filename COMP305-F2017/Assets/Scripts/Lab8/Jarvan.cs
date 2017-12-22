759136bbd6594c16a742984dfdc4c7a4e7ad3c7a
Mr. Marvin Jupiter Roilo Vargas <mvargas2@my.centennialcollege.ca>
2017-12-22 15:06:44 -0500

    Completed Lab8

---
+++ COMP305-F2017/Assets/Scripts/Lab8/Jarvan.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jarvan : MonoBehaviour {
+    public Transform start;
+    public Transform end;
+    public float speed = 5;
+
+    private float rotateSpeed = 5;
+    private Rigidbody2D rBody;
+
+    void Start()
+    {
+        transform.position = start.position;
+        rBody = this.GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+
+      
+        Vector3 vectorToTarget = end.position - transform.position;
+        float angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg;
+        Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = Quaternion.Slerp(transform.rotation, q, Time.deltaTime * rotateSpeed);
+
+ 
+ 
+        transform.position += transform.right * Time.deltaTime * speed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Vector3 newAngle = transform.eulerAngles;
+        newAngle.z += 100;
+        transform.eulerAngles = newAngle;
+    }
+}  // https://answers.unity.com/questions/650460/rotating-a-2d-sprite-to-face-a-target-on-a-single.html
+   // https://docs.unity3d.com/ScriptReference/Vector3.MoveTowards.html
+
---
+++ COMP305-F2017/Assets/Scripts/Lab8/ObstacleSpawn.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine;
+
+
+public class ObstacleSpawn : MonoBehaviour {
+    public float spawnDelay = 1f;
+    public Camera cam;
+    public GameObject selectPanel;
+    public GameObject selectButtonPrefab;
+    public GameObject[] objsToSpawn;
+
+    private string buttonAxis = "Fire1";
+    private float timeSinceSpawn;
+    private int selectedIndex = 0;
+
+    void Start()
+    {
+        timeSinceSpawn = spawnDelay;
+
+        for (int i = 0; i < objsToSpawn.Length; i++)
+        {
+            GameObject button = Instantiate(selectButtonPrefab, selectPanel.transform);
+
+            button.GetComponent<Image>().sprite = objsToSpawn[i].GetComponent<SpriteRenderer>().sprite;
+
+            int index = i;
+            button.GetComponent<Button>().onClick.AddListener(() => {
+                SetSelectedIndex(index);
+            });
+        }
+    }
+
+    void Update()
+    {
+        timeSinceSpawn += Time.deltaTime;
+
+        if (timeSinceSpawn > spawnDelay && Input.GetAxisRaw(buttonAxis) > 0
+                && !EventSystem.current.IsPointerOverGameObject())
+        {
+            SpawnObject();
+            timeSinceSpawn = 0.0f;
+        }
+    }
+
+    private void SpawnObject()
+    {
+        Vector3 cursorInWorldPos = cam.ScreenToWorldPoint(Input.mousePosition);
+        cursorInWorldPos.z = 0;
+        Instantiate(objsToSpawn[selectedIndex], cursorInWorldPos, Quaternion.identity, this.transform);
+    }
+
+    public void SetSelectedIndex(int newIndex)
+    {
+        selectedIndex = newIndex;
+    }
+}//AbbyNode
